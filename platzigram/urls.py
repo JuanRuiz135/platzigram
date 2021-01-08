f00b77e38a9f1605cdb4e4555cc5af7adcf74707
Juan <juandevss@gmail.com>
2021-01-07 22:51:03 -0500

    added login and logout options

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -11,14 +11,20 @@ from django.conf.urls.static import static
 # local urls
 from platzigram import views as local_views
 from posts import views as posts_views
+from users import views as users_views
+
 
 
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
 
-    path('hi/', local_views.hi),
-    path('posts/', posts_views.list_posts),
+    path('hi/', local_views.hi, name='hi'),
+    path('posts/', posts_views.list_posts, name='feed'),
+
+    path('users/login/', users_views.login_view, name='login'),
+    path('users/logout/', users_views.logout_view, name='logout'),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
